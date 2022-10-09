9727e48bf76ab14973cac323141f453f8be84179
Abhay Ganti <abhayganti0909@gmail.com>
2022-10-09 14:39:22 +0800

    Add user-input functionality

--- text_summariser.py
+++ text_summariser.py
@@ -7,7 +7,7 @@ nltk.download('punkt')
 nltk.download('stopwords')
 
 # Preprocessing data
-para = "Exploratory data analysis (EDA) is used by data scientists to analyze and investigate data sets and summarize their main characteristics, often employing data visualization methods. It helps determine how best to manipulate data sources to get the answers you need, making it easier for data scientists to discover patterns, spot anomalies, test a hypothesis, or check assumptions.EDA is primarily used to see what data can reveal beyond the formal modeling or hypothesis testing task and provides a provides a better understanding of data set variables and the relationships between them. It can also help determine if the statistical techniques you are considering for data analysis are appropriate. Originally developed by American mathematician John Tukey in the 1970s, EDA techniques continue to be a widely used method in the data discovery process today." # Template text
+para = str(input('Enter a paragraph(s): '))
 sentences = nltk.sent_tokenize(para)
 
 words = []
@@ -49,6 +49,8 @@ for sentence in sentences:
                       sentence_score[sentence] += weighted_frequency[word]
 print(sentence_score)
 
-summary_sentences = heapq.nlargest(5, sentence_score, key=sentence_score.get)
+lines = int(input('Enter the number of lines: '))
+
+summary_sentences = heapq.nlargest(lines, sentence_score, key=sentence_score.get)
 summary = ' '.join(summary_sentences)
 print(summary)
